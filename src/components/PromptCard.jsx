3b847e660209644146e0b6d49b3822599d1b12d6
SamWraith <samirnandan2003@gmail.com>
2024-07-30 17:39:31 +0530

    added some checks for image

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -32,18 +32,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                     onClick={handleProfileClick}
                 >
                     <Image
-                        src={post.creater.image}
-                        alt="user_image"
+                        src={post?.creater?.image }
+                        alt={post?.creater?.username}
                         width={40}
                         height={40}
                         className="rounded-full object-contain"
                     />
                     <div className="flex flex-col">
                         <h3 className="font-satoshi font-semibold text-gray-900">
-                            {post.creater.username}
+                            {post?.creater?.username}
                         </h3>
                         <p className="font-inter text-sm text-gray-500">
-                            {post.creater.email}
+                            {post?.creater?.email}
                         </p>
                     </div>
                 </div>
@@ -61,7 +61,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 </div>
             </div>
             <p className="my-4 font-satoshi text-sm text-gray-700">
-                {post.prompt}
+                {post?.prompt}
             </p>
             <p
                 className="font-inter text-sm blue_gradient cursor-pointer"
@@ -70,7 +70,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 {post.tag}
             </p>
 
-            {session?.user.id === post.creater._id &&
+            {session?.user.id === post?.creater?._id &&
                 pathName === "/profile" && (
                     <div className="mt-5 flex-center gap-4 border-t  border-gray-100 pt-3">
                         <p
